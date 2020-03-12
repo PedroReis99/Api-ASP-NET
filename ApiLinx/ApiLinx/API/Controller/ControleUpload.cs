40745291a0b273d1f71036293fb1770381363c52
PedroReis99 <pmreis6@gmail.com>
2020-03-12 15:04:02 -0300

    API
    Api feita em Net core modificada

---
+++ ApiLinx/ApiLinx/API/Controller/ControleUpload.cs
@@ -0,0 +1,47 @@
+﻿using ApiLinx.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiLinx.API.Data;
+using Microsoft.AspNetCore.Cors;
+
+namespace ApiLinx.API.Controller
+{
+    [EnableCors("AllowSpecificOrigin")]
+    [ApiController]
+    [Route("")]
+    public class ControleItem :ControllerBase
+    {
+ 
+        [HttpGet]
+        [Route("insert/produto")]
+
+        public async Task<ActionResult<List<ProdutoUpload>>> Get([FromServices] Data.DataContext context)
+        {
+            var produto = await context.Produtos.ToListAsync();
+            return produto;
+        }
+
+        [HttpPost]
+        [Route("request/produto")]
+
+        public async Task<ActionResult<ProdutoUpload>> Post(
+            [FromServices] DataContext context,
+            [FromBody] ProdutoUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Produtos.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
